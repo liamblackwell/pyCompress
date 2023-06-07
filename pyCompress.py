13b4d2dd659e242e163bf3dea8ee0e6eb6dd2a1e
liamblackwell <liamb.grapes@gmail.com>
2023-06-07 20:04:33 +0800

    refactored python components

--- pyCompress.py
+++ pyCompress.py
@@ -1,148 +1,87 @@
-# Program for compressing videos using a tkinter GUI and ffmpeg
-from tkinter import *
-from tkinter import filedialog
-import subprocess
-import sys
-import os
+from tkinter import filedialog, Label, Button, Entry, Tk, OptionMenu, StringVar
+from sys import exit
+from os import path, mkdir
+from ctypes import CDLL
 
-print("Opening GUI...")
+compressPath = "./c_files/compress.so"
+compress = CDLL(compressPath)
 
-if not os.path.exists("output"):
-        os.mkdir("output")
+if not path.exists("output"):
+        mkdir("output")
 
 #use global variables so that information can be remembered between runs of compression function
+lastLocation = './'
 outputDest = './output' 
 filename = ''
 filename2 = ''
 
-# Function for opening the file explorer window
+# Functions for opening file explorer windows
 def browseInput():
     global filename
-    filename = filedialog.askopenfilename(initialdir = filename + "/", #open at last place used by browser
+    global lastLocation
+    filename = filedialog.askopenfilename(initialdir = lastLocation + "/", #open at last place used by browser
                                           title = "Select a File",
-                                          filetypes = ((".mp4",
-                                                        "*.mp4*"),
-                                                       ("all files",
-                                                        "*.*")))
-    
+                                          filetypes = ((".mp4", "*.mp4*"), ("all files", "*.*")))
+    #Update last used location
+    lastLocation = path.dirname(filename)
     # Change label contents
     label_inputPath1.configure(text="File opened: " + filename)
 
 def browseInputTwo():
     global filename2
-    filename2 = filedialog.askopenfilename(initialdir = filename2 + "/", #open at last place used by browser
+    global lastLocation
+    filename2 = filedialog.askopenfilename(initialdir = lastLocation + "/", #open at last place used by browser
                                           title = "Select a File",
-                                          filetypes = ((".mp4",
-                                                        "*.mp4*"),
-                                                       ("all files",
-                                                        "*.*")))
-    
+                                          filetypes = ((".mp4", "*.mp4*"), ("all files", "*.*")))
+    #Update last used location
+    lastLocation = path.dirname(filename2)
     # Change label contents
     label_inputPath2.configure(text="File opened: " + filename2)
 
 def browseOutput():
     global outputDest
-    outputDest = filedialog.askdirectory(initialdir = outputDest, #open at last place used by browser
+    global lastLocation
+    outputDest = filedialog.askdirectory(initialdir = lastLocation, #open at last place used by browser
                                           title = "Select an output folder")
-    
+    #Update last used location
+    lastLocation = path.dirname(outputDest)
     # Change label contents
     label_outputPath.configure(text="Output path: " + outputDest)
 
-#Compresses one video without target size
-def compressVideo():
-    global filename
-    global outputDest
+#Calls C function to compress
+def compressButton():
+     global filename
+     print(f"input {filename} length {len(filename)}")
+     global outputDest
+     filenameClean = filename.replace(' ', '\ ').encode()
+     outputDestClean = outputDest.replace(' ', '\ ').encode()
+     outputName = entry_outputName.get().replace(' ', '\ ').encode()
+     outputSize = entry_outputSize.get()
+     preset = presetVar.get().encode()
 
-    preset = presetVar.get()
-
-    if filename == "":
+     if filename == '':
         print("ERROR - NO INPUT FILE.")
         return
-
-    if not os.path.exists("output"): #check again for output folder in case it was deleted while the program was running
-        os.mkdir("output")
-
-    outputName = entry_outputName.get() #set name of output, or set to a default if none given
-    if outputName == '':
-        outputName = os.path.basename(filename) + "_Compressed"
-
-    subprocess.run(["ffmpeg", "-i", filename, "-c:v", "libx264", "-preset", preset, "-crf", "22", "-c:a", "copy", f"{outputDest}/{outputName}.mp4"], shell=True)
-
-    print("------------------")
-    print("Video compressed!")
-
-#Compresses video to target size, potentially losing quality
-def resize():
-    global filename
-    global outputDest
-
-    preset = presetVar.get()
-    #inputSize = sys.getsizeof(filename) #returns bytes
-    outputSize = float(entry_outputSize.get())
-
-    #audioBitrate = 64 if outputSize < 0.15  else 128 #reduces audio bitrate if outputting below certain size
-    audioBitrate = float(entry_audioBitrate.get())
-
-    #validation
-    if audioBitrate > 512:
-        print("Audio bitrate too high! Maximum bitrate is 512kbps.")
-        return
-    if filename == "":
-        print("ERROR - NO INPUT FILE.")
-        return
-    outputName = entry_outputName.get() #set name of output, or set to a default if none given
-    if outputName == '':
-        outputName = os.path.basename(filename) + "_Compressed"
-    if not os.path.exists("output"): #make 'output' directory if not present
-        os.mkdir("output")
     
-    subprocess.run(["./compress", f"-t {outputSize}", filename, outputDest, outputName, preset])
+     if not outputSize == '' and float(outputSize) > 0:
+        compress.twoPass(filenameClean, outputDestClean, outputName, outputSize, preset)
+     else: compress.simple(filenameClean, outputDestClean, outputName, preset)
 
+#Stitch 2 video files together
+#TODO - DOES NOT WORK
+def concatenate(filename, filename2, outputName, outputDest):
     '''
-    duration = VideoFileClip(filename).duration
-    minSize = (audioBitrate * duration) / 8000
-
-    if outputSize < minSize: audioBitrate = 64
-
-    minSize = (audioBitrate * duration) / 8000
-
-    if outputSize < minSize: #check if given size is too small for 
-        print(f"Requested size is too small! Minimum size for this file: {minSize} MB")
-        return
-    
-    bitrate = ((outputSize * 8000 / duration) - audioBitrate) #get output video bitrate by converting size given in MB to kilobits, divide by length, and account for audio bitrate
-
-    #First pass
-    subprocess.run(["ffmpeg", "-y", "-i", filename, "-max_muxing_queue_size", "9999", "-c:v", "libx264", "-preset", preset, "-b:v", f"{bitrate}k", "-pass", "1", 
-                    "-vsync", "cfr", "-f", "null", "/dev/null", "&&", '\''], shell=True)
-    #Second pass
-    subprocess.run(["ffmpeg", "-i", filename, "-max_muxing_queue_size", "9999", "-c:v", "libx264", "-preset", preset, "-b:v", f"{bitrate}k", "-pass", "2", 
-                    "-c:a", "aac", "-b:a", f"{audioBitrate}k", f"{outputDest}/{outputName}.mp4"], shell=True)
-
-    # First pass: ffmpeg -y -i input -max_muxing_queue_size 9999 -c:v libx264 -b:v 2600k -pass 1 -an -f null /dev/null && \
-    # Second pass: ffmpeg -i input -max_muxing_queue_size 9999 -c:v libx264 -b:v 2600k -pass 2 -c:a aac -b:a 128k output.mp4
-
-    print("------------------")
-    print("Video compressed!")
+    #run(["ffmpeg", "-i", f"{filename}", "-c", "copy", "intermediate1.ts"], shell=True) #make temp file 1
+    #run(["ffmpeg", "-i", f"{filename2}", "-c", "copy", "intermediate2.ts"], shell=True) #make temp file 2
+    #run(["ffmpeg", "-i", "\"concat:intermediate1.ts|intermediate2.ts\"", "-c", "copy", f"{outputDest}/{outputName}.mp4"], shell=True) #runs concat command
+    run(["mkfifo temp1 temp2"], shell=True)
+    run([f"ffmpeg -y -i {filename} -c copy -bsf:v h264_mp4toannexb -f mpegts temp1 2> /dev/null & \ "], shell=True)
+    run([f"ffmpeg -y -i {filename2} -c copy -bsf:v h264_mp4toannexb -f mpegts temp2 2> /dev/null & \ "], shell=True)
+    run([f"ffmpeg -f mpegts -i \"concat:temp1|temp2\" -c copy -bsf:a aac_adtstoasc {outputDest}/{outputName}.mp4"], shell=True)
     '''
 
-def execute():
-    outputSize = entry_outputSize.get()
-    if not outputSize == '' and float(outputSize) > 0:
-        resize()
-    else: compressVideo()
-
-def concatenate():
-    global outputDest
-    outputName = entry_outputName.get()
-    #subprocess.run(["ffmpeg", "-i", f"{filename}", "-c", "copy", "intermediate1.ts"], shell=True) #make temp file 1
-    #subprocess.run(["ffmpeg", "-i", f"{filename2}", "-c", "copy", "intermediate2.ts"], shell=True) #make temp file 2
-    #subprocess.run(["ffmpeg", "-i", "\"concat:intermediate1.ts|intermediate2.ts\"", "-c", "copy", f"{outputDest}/{outputName}.mp4"], shell=True) #runs concat command
-    subprocess.run(["mkfifo temp1 temp2"], shell=True)
-    subprocess.run([f"ffmpeg -y -i {filename} -c copy -bsf:v h264_mp4toannexb -f mpegts temp1 2> /dev/null & \ "], shell=True)
-    subprocess.run([f"ffmpeg -y -i {filename2} -c copy -bsf:v h264_mp4toannexb -f mpegts temp2 2> /dev/null & \ "], shell=True)
-    subprocess.run([f"ffmpeg -f mpegts -i \"concat:temp1|temp2\" -c copy -bsf:a aac_adtstoasc {outputDest}/{outputName}.mp4"], shell=True)
-
+#TK WINDOW ELEMENT DECLARATIONS
+#region
 # Create the root window
 window = Tk()
 # Set window title
@@ -151,7 +90,7 @@ window.title('pyCompress!')
 window.geometry("700x700")
 #Set window background color
 window.config(background = "white")
-  
+
 #Title label
 label_title = Label(window,
                     text = "Video compressor using ffmpeg and Tkinter",
@@ -214,22 +153,24 @@ label_presets = Label(window,
 #Dropdown menu of compression presets
 presetList = ['ultrafast', 'superfast', 'veryfast', 'faster', 'fast', 'medium', 'slow', 'slower', 'veryslow']
 presetVar = StringVar()
-presetVar.set(presetList[5])
+presetVar.set(presetList[2])
 options_presets = OptionMenu(window,
                             presetVar,
                             *presetList)
+
+#BUTTONS
 #Button to run compressing process
 button_compress = Button(window,
                         text = "Compress Video",
-                        command = execute)
+                        command = compressButton)
 #Button to run compressing process
 button_concat = Button(window,
                         text = "Concatenate Video",
                         command = concatenate)
 #Button to exit program
 button_exit = Button(window,
-                     text = "Exit",
-                     command = sys.exit)
+                    text = "Exit",
+                    command = exit)
 
 # Grid method is chosen for placing the widgets at respective positions in a table like structure by specifying rows and columns
 label_title.grid(column = 1, row = 1, pady = [0, 3])
@@ -250,6 +191,8 @@ options_presets.grid(column = 1, row = 15, pady = [1, 3])
 button_compress.grid(column = 1, row = 16, pady = [10, 3])
 button_concat.grid(column = 1, row = 17, pady = [10, 3])
 button_exit.grid(column = 1,row = 18, pady = [10, 3])
+#endregion
 
 # Let the window wait for any events
+print("Opening GUI...")
 window.mainloop()
